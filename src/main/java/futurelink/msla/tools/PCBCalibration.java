b817e1e126000e8920f29d4c895c96005b74b9d0
Denis Pavlov <Denis.Pavlov@dataart.com>
2023-06-23 15:07:09 +0200

    PCB calibration pattern generator

---
+++ src/main/java/futurelink/msla/tools/PCBCalibration.java
@@ -0,0 +1,101 @@
+package futurelink.msla.tools;
+
+import futurelink.Main;
+import futurelink.msla.formats.MSLAEncodeReader;
+import futurelink.msla.formats.MSLAFile;
+import futurelink.msla.formats.MSLAFileCodec;
+import futurelink.msla.formats.anycubic.PhotonWorkshopFile;
+import futurelink.msla.formats.anycubic.PhotonWorkshopFileDefaults;
+
+import java.awt.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class PCBCalibration {
+    /**
+     * Creates a PCB photo resistive film calibration pattern.
+     *
+     * @param machineName model of a mSLA 3D printing machine
+     * @param startTime initial curing time
+     * @param interval curing time interval
+     * @param repetitions number of samples or intervals
+     * @throws IOException
+     */
+    public static void generateTestPattern(String machineName, String fileName, float startTime, float interval, int repetitions) throws IOException {
+        var defaults = PhotonWorkshopFileDefaults.get(machineName);
+        if (defaults == null) throw new IOException("Unknown machine name: " + machineName);
+        try (var fos = new FileOutputStream(fileName + "." + defaults.getFileExtension())) {
+            var wsFile = new PhotonWorkshopFile(defaults);
+            var wsHeader = wsFile.getHeader();
+            var wsExtra =  wsFile.getExtra();
+            if ((wsHeader == null) || (wsExtra == null)) throw new IOException("File was not initialized properly");
+
+            // Set options
+            wsHeader.setPerLayerOverride(1);
+            wsHeader.setBottomLayersCount(1);
+            wsHeader.setBottomExposureTime(startTime);
+            wsHeader.setExposureTime(interval);
+            wsHeader.setLiftHeight(0.5f);
+            wsHeader.setTransitionLayers(0);
+
+            wsExtra.setLiftHeight1(0.5f);
+            wsExtra.setLiftHeight2(0.5f);
+            wsExtra.setBottomLiftHeight1(0.5f);
+            wsExtra.setBottomLiftHeight2(0.5f);
+
+            // Create preview image
+            createPreview(wsFile);
+
+            // Generate pattern layers
+            var pattern = new PCBCalibrationPattern(wsHeader.getResolutionX(), wsHeader.getResolutionY(),
+                    wsHeader.getPixelSizeUm());
+            pattern.setStartTime(startTime);
+            var encoder = new MSLAEncodeReader() {
+                volatile int layersLeft = repetitions;
+                @Override public MSLAFileCodec getCodec() {
+                    return wsFile.getCodec();
+                }
+                @Override public InputStream read(int layerNumber) throws IOException {
+                    var image = pattern.generate(wsHeader.getResolutionY() / 3, layerNumber, repetitions);
+                    var raster = image.getRaster();
+                    return new Main.RasterBytesInputStream(raster);
+                }
+                @Override public void onStart(int layerNumber) {}
+                @Override public void onFinish(int layerNumber, int pixels, int length) {
+                    System.out.println("Layer " + layerNumber + " done: " + pixels + " pixels, " + length + " bytes");
+                    synchronized (wsFile) { layersLeft--; }
+                }
+                @Override public void onError(int layerNumber, String error) {
+                    System.out.println("Layer " + layerNumber + " error: " + error);
+                    synchronized (wsFile) { layersLeft--; }
+                }
+            };
+            for (int i = 0; i < repetitions; i++) wsFile.addLayer(
+                    encoder, 0.05f, interval, 10.0f, 0.5f
+            );
+            while (encoder.layersLeft > 0); // Wait until all layers are encoded
+            wsFile.write(fos);
+        }
+    }
+
+    public static void createPreview(MSLAFile file) throws IOException {
+        var preview = file.getPreview();
+        if (preview != null) {
+            var graphics = preview.getImage().getGraphics();
+            graphics.setFont(graphics.getFont().deriveFont(20.0f));
+            graphics.setColor(Color.WHITE);
+            graphics.drawRoundRect(10, 10,
+                    preview.getResolutionX() - 20,
+                    preview.getResolutionY() - 20,
+                    10, 10);
+            graphics.setColor(Color.WHITE);
+            graphics.drawString("PCB Test pattern", 16, 16 + 20);
+            graphics.setFont(graphics.getFont().deriveFont(16.0f));
+            graphics.setColor(Color.RED);
+            graphics.drawString("DO NOT PRINT!", 16, 16 + 20 + 30);
+
+            file.updatePreviewImage();
+        }
+    }
+}
---
+++ src/main/java/futurelink/msla/tools/PCBCalibrationPattern.java
@@ -0,0 +1,105 @@
+package futurelink.msla.tools;
+
+import lombok.Setter;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ *                   Pattern example (one character 0.1mm width):
+ *   6 layers, 2 seconds each layer the last one appear on all layer images,
+ *   the first one is displayed only once in 1st layer. Distance between copper lines
+ *   is 0.1mm minimum and 0.3mm max, enough to test.
+ *   ---------------------------------------------------------------------------------
+ *   | | | ||  ||  ||  |||
+ *   | | | ||  ||  ||  |||
+ *   | | | ||  ||  ||  |||
+ *   | | | ||  ||  ||  |||
+ *   | | | ||  ||  ||  |||
+ *   ||| | | ||  |  |   ||
+ *   ||| | | ||  |  |   ||
+ *   ||| | | ||  |  |   ||
+ *   ||| | | ||  |  |   ||
+ *   ||| | | ||  |  |   ||
+ *   ||| ||| ||  | ||  |||
+ *   ||| ||| ||  | ||  |||
+ *   ||| ||| ||  | ||  |||
+ *   ||| ||| ||  | ||  |||
+ *   ||| ||| ||  | ||  |||
+ *             5s                   7s                 9s                 11s
+ *   ---------------------------------------------------------------------------------
+ */
+public class PCBCalibrationPattern {
+    @Setter int height; // in mm
+    float pixelSize;
+    int resolutionX;
+    int resolutionY;
+
+    @Setter float startTime = 5.0f;
+    @Setter float interval = 1.0f;
+
+    // Pattern widths (tracks) - evens are line widths, odds are gap widths
+    float[][] pattern = {
+            { 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.3f },
+            { 0.3f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.2f, 0.2f, 0.1f, 0.2f, 0.1f, 0.3f, 0.2f },
+            { 0.3f, 0.1f, 0.3f, 0.1f, 0.2f, 0.2f, 0.1f, 0.1f, 0.2f, 0.2f, 0.3f }
+    };
+
+    // Circles (THT pads) - first is outer diameter, second is inner diameter
+    float[][] circles = {
+            {0.3f, 0.15f}, {0.4f, 0.2f}, {0.5f, 0.25f}, { 0.6f, 0.3f },
+            { 0.8f, 0.4f }, { 0.9f, 0.45f }, { 1.0f, 0.5f }
+    };
+
+    public PCBCalibrationPattern(int resolutionX, int resolutionY, float pixelSize) {
+        this.resolutionX = resolutionX;
+        this.resolutionY = resolutionY;
+        this.pixelSize = pixelSize;
+    }
+
+    public BufferedImage generate(int height, int repetition, int repetitions) throws IOException {
+        var image = new BufferedImage(resolutionX, resolutionY, BufferedImage.TYPE_BYTE_GRAY);
+        var g = image.getGraphics();
+
+        // Draw top one third of pattern 0.1 + 0.1 + 0.2 + 0.2 + 0.3mm = 0.9mm total
+        var distancePX = 120;
+        var widthPX = distancePX * repetitions;
+        var posX = resolutionX / 2 - widthPX / 2;
+        var posY = resolutionY / 2 - height / 2;
+        var fragmentHeight = height / 3;
+        for (int i = 0; i < repetitions - repetition; i++) {
+            // Draw lines
+            g.setColor(Color.WHITE);
+            for (int p = 0; p < 3; p++) {
+                var offset = i * distancePX;
+                for (int j = 0; j < pattern[p].length; j++) {
+                    var w = pattern[p][j] / pixelSize * 1000;
+                    if ((j % 2) == 0) { // Draw road
+                        g.fillRect(offset + posX, posY + fragmentHeight * p, (int) Math.floor(w), fragmentHeight);
+                    }
+                    offset += w;
+                }
+            }
+
+            // Draw circles
+            for (int p = 0; p < circles.length; p++) {
+                var ro = (int) Math.floor(circles[p][0] * 500 / pixelSize); // A circle radius
+                var x = posX + i * distancePX + 80 - ro;
+                var y = posY + p * 50 - ro + 100;
+                g.setColor(Color.WHITE);
+                g.fillOval(x, y, ro * 2, ro * 2);
+
+                var ri = (int) Math.floor(circles[p][1] * 500 / pixelSize); // A hole radius
+                g.setColor(Color.BLACK);
+                g.fillOval(x + ro - ri, y + ro - ri, ri * 2, ri * 2);
+            }
+            // Add text
+            g.setColor(Color.WHITE);
+            g.setFont(g.getFont().deriveFont(40.0f));
+            g.drawString((startTime + interval * (repetitions - i - 1) )+ "s", posX + i * distancePX, posY + height + 40);
+        }
+
+        return image;
+    }
+}
